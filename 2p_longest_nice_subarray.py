791d7bc89074740a468306188a7b8912ef218375
Pushkar Garg <pushkar@appyhigh.com>
2024-04-23 11:39:03 +0530

    Longest Nice Subarray

---
+++ 2p_longest_nice_subarray.py
@@ -0,0 +1,42 @@
+# Longest Nice Subarray
+
+# You are given an array nums consisting of positive integers.
+# We call a subarray of nums nice if the bitwise AND of every pair of elements that are in different positions in the subarray is equal to 0.
+# Return the length of the longest nice subarray.
+# A subarray is a contiguous part of an array.
+# Note that subarrays of length 1 are always considered nice.
+ 
+# Example 1:
+# Input: nums = [1,3,8,48,10]
+# Output: 3
+# Explanation: The longest nice subarray is [3,8,48]. This subarray satisfies the conditions:
+# - 3 AND 8 = 0.
+# - 3 AND 48 = 0.
+# - 8 AND 48 = 0.
+# It can be proven that no longer nice subarray can be obtained, so we return 3.
+
+# Example 2:
+# Input: nums = [3,1,5,11,13]
+# Output: 1
+# Explanation: The length of the longest nice subarray is 1. Any subarray of length 1 can be chosen.
+
+nums = [1,3,8,48,10]
+i = 0
+j = 0
+n = len(nums)
+ans = 0
+bit_mask = 0
+while j < n:
+    if nums[j] & bit_mask == 0:
+        ans = max(j-i+1, ans)
+        print(i, j)
+        bit_mask = bit_mask | nums[j]
+        j+=1
+    else:
+        while i <=j and nums[j] & bit_mask:
+            bit_mask = bit_mask ^ nums[i]
+            i+=1
+        # j+=1
+
+
+print(ans)
